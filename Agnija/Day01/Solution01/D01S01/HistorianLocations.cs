0d35be40d9aa9c0f794235ca8008dd864e75423b
agnija-bako <agnijabako@yahoo.com>
2024-12-01 21:25:36 +0100

    feat: solution1

---
+++ Agnija/Day01/Solution01/D01S01/HistorianLocations.cs
@@ -0,0 +1,38 @@
+﻿namespace D01S01;
+
+public static class HistorianLocations
+{
+    public static int FindLocationIdDifferences(List<int> list1, List<int> list2)
+    {
+        var totalDifference = 0;
+        var itemCount = list1.Count;
+        for (var i = 0; i < itemCount; i++)
+        {
+
+            var smallestList1 = FindSmallestElement(list1);
+            list1.Remove(smallestList1);
+
+            var smallestList2 = FindSmallestElement(list2);
+
+            list2.Remove(smallestList2);
+            totalDifference += Math.Abs(smallestList1 - smallestList2);
+        }
+
+        return totalDifference;
+    }
+
+    private static int FindSmallestElement(List<int> elements)
+    {
+        var smallestElement = elements[0];
+        foreach (var el in elements)
+        {
+            if (el < smallestElement)
+            {
+                smallestElement = el;
+
+            }
+        }
+
+        return smallestElement;
+    }
+}
---
+++ Agnija/Day01/Solution01/D01S01/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using D01S01;
+
+var solutionDirectory = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName!;
+var filePath = Path.Combine(solutionDirectory, "Resources", "Input");
+string[] lines = File.ReadAllLines(filePath);
+
+
+var list1 = new List<int>();
+var list2 = new List<int>();
+
+foreach (var line in lines)
+{
+    string[] parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length == 2 && int.TryParse(parts[0], out int num1) && int.TryParse(parts[1], out int num2))
+    {
+        list1.Add(num1);
+        list2.Add(num2);
+    }
+}
+
+var totalDifference = HistorianLocations.FindLocationIdDifferences(list1, list2);
+
+
+Console.WriteLine($"Total difference: {totalDifference}");
+
+
+

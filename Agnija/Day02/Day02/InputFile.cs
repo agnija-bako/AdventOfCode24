78f23debb1d2c10e35d80b2eeab08f8406f08f8b
agnija-bako <agnijabako@yahoo.com>
2024-12-02 21:09:50 +0100

    feat: solution day 2

---
+++ Agnija/Day02/Day02/InputFile.cs
@@ -0,0 +1,32 @@
+﻿namespace Day02;
+
+public static class InputFile
+{
+    public static List<List<int>> GetInput()
+    {
+        var solutionDirectory = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName!;
+        var filePath = Path.Combine(solutionDirectory, "Resources", "Input.txt");
+
+        var lists = new List<List<int>>();
+
+        var lines = File.ReadAllLines(filePath);
+
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ');
+
+            var currentList = new List<int>();
+            foreach (string part in parts)
+            {
+                if (int.TryParse(part, out int number))
+                {
+                    currentList.Add(number);
+                }
+            }
+
+            lists.Add(currentList);
+        }
+
+        return lists;
+    }
+}
---
+++ Agnija/Day02/Day02/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day02;
+
+var lists = InputFile.GetInput();
+
+var totalSafeReports = 0;
+
+foreach (var list in lists)
+{
+    var increasing = (list[0] < list[1]);
+
+    var match = 1;
+    for (var i = 0; i < list.Count - 1; i++)
+    {
+        var diff = list[i] - list[i + 1];
+        if (diff == 0)
+            continue;
+
+        if (increasing)
+        {
+            if (diff is >= -3 and < 0)
+            {
+                match++;
+            }
+
+            continue;
+        }
+
+        if (diff is <= 3 and > 0)
+        {
+            match++;
+        }
+    }
+
+    if (list.Count == match || list.Count -1 == match) //Solution 1 + solution 2
+    {
+        totalSafeReports++;
+    }
+}
+
+Console.WriteLine($"Total safe reports: {totalSafeReports}");

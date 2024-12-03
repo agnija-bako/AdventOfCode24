73c67c432fbcf12f5050651dfb781a12d197aff0
agnija-bako <agnijabako@yahoo.com>
2024-12-03 21:06:48 +0100

    feat: day 3 solutino

---
+++ Agnija/Day03/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+
+var solutionDirectory = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName!;
+var filePath = Path.Combine(solutionDirectory, "Resources", "Input.txt");
+var lines = File.ReadAllLines(filePath);
+
+var total = 0;
+var pattern = @"mul\((\d+),(\d+)\)";
+foreach (var line in lines)
+{
+    var matches = Regex.Matches(line, pattern);
+    foreach (Match match in matches)
+    {
+        var firstDigit = match.Groups[1].Value;
+        var secondDigit = match.Groups[2].Value;
+
+        total += (int.Parse(firstDigit) * int.Parse(secondDigit));
+    }
+    
+}
+Console.WriteLine($"Total: {total}");

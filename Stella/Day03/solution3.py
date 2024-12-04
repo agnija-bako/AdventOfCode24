a4a99ad96aae2cbbd63f9c822f206d256563265b
StellaCea <stela.ceaicovscaia@hotmail.com>
2024-12-04 22:38:09 +0100

    solution3 part2

--- Stella/Day03/solution3.py
+++ Stella/Day03/solution3.py
@@ -5,18 +5,27 @@ def find_mul(filename):
     with open(filename, 'r') as file:
         data = file.read()
 
-    pattern = r"mul\((\d+)\s*,\s*(\d+)\)"
+    pattern = r"(mul\((\d+),(\d+)\)|don't\(\)|do\(\))"
     matches = re.findall(pattern, data)
 
     #Calculate the sum of multiplications
     sum = 0
+    enabled = True
+
     for match in matches:
-        a, b = int(match[0]), int(match[1])
-        sum += a * b
+        instruction, a ,b = match
+        if instruction == "don't()":
+            enabled = False
+        elif instruction == "do()":
+            enabled = True
+        elif instruction.startswith("mul") and enabled:
+            sum += int(a) * int(b)
     return sum
+
+
 
 filename = "input.txt"
 result = find_mul(filename)
-print("The sum of all multiplications is: ", result)
+print("The sum of enabled multiplications is: ", result)
 
 

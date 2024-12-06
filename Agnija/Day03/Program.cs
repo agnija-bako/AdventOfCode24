d2686bf446b2d45fecafd11110a61a08e8c81dd2
agnija-bako <agnijabako@yahoo.com>
2024-12-06 22:47:35 +0100

    fix: day03 solution02

--- Agnija/Day03/Program.cs
+++ Agnija/Day03/Program.cs
@@ -6,19 +6,33 @@ using System.Text.RegularExpressions;
 var solutionDirectory = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName!;
 var filePath = Path.Combine(solutionDirectory, "Resources", "Input.txt");
 var lines = File.ReadAllLines(filePath);
+var input    = string.Join(string.Empty, lines);
 
-var total = 0;
-var pattern = @"mul\((\d+),(\d+)\)";
-foreach (var line in lines)
+//solution 2
+const string disposePattern = @"(?<=don't\(\)).*?(?=do\(\))";
+var output = Regex.Replace(input, disposePattern, string.Empty); //clear the useless shit
+
+var lastDoIndex = output.LastIndexOf("do()", StringComparison.Ordinal); //clear everything after first don't() following the last do()
+if (lastDoIndex != -1)
 {
-    var matches = Regex.Matches(line, pattern);
-    foreach (Match match in matches)
+    var orphanDontIndex = output.IndexOf("don't()", lastDoIndex, StringComparison.Ordinal);
+    if (orphanDontIndex != -1)
     {
-        var firstDigit = match.Groups[1].Value;
-        var secondDigit = match.Groups[2].Value;
-
-        total += (int.Parse(firstDigit) * int.Parse(secondDigit));
+        output = output[..orphanDontIndex];
     }
-    
 }
+
+//solution 1
+var total = 0;
+const string pattern = @"mul\((\d+),(\d+)\)";
+
+var matches = Regex.Matches(output, pattern);
+foreach (Match match in matches)
+{
+    var firstDigit = match.Groups[1].Value;
+    var secondDigit = match.Groups[2].Value;
+
+    total += (int.Parse(firstDigit) * int.Parse(secondDigit));
+}
+
 Console.WriteLine($"Total: {total}");
